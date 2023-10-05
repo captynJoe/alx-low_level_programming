706b876aa44bdfff78febeb2cf4f06058204d621
captynJoe <joelnjeng@gmail.com>
2023-10-05 12:05:26 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long: %zu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
-return (0);
+return (1);
 
 }
 
