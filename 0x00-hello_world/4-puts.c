dda723d18a43c23a5b99969747016b9b41f8f282
captynJoe <joelnjeng@gmail.com>
2023-10-05 09:20:20 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+include <stdio.h>
+
+/**
+ * main -prints "Programming is like building a multilingual
+ * puzzle"
+ * return 0
+ */
+
+int main()
+{
+puts("\"programming is like building a multilingual puzzle");
+return(0);
+}
