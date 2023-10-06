3ca6446aa5a4ab751c68b7ebee37b79ba937f30a
captynJoe <joelnjeng@gmail.com>
2023-10-06 15:09:41 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,8 +13,8 @@ int main(void)
         for (i = 'a' ; i <= 'z' ; i++)
                 putchar(i);
 	for (i = 'A' ; i <= 'Z' ; i++)
-        	putchar(i);
-	putchar('\n');
+		putchar(i);
+        putchar('\n');
         return (0);
 
 }
