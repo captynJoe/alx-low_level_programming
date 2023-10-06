f17ace8e3795b3cb99a3ecef6680b6ce7ab6c327
captynJoe <joelnjeng@gmail.com>
2023-10-06 16:25:08 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function task9
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0 ; i < 10 ; i++)
+{
+	putchar(i);
+	putchar(',');
+	putchar(' ');
+
+}
+putchar('\n');
+return(0);
+
+}
