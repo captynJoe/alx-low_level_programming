633acfe667f91e0b071d507e66ab756b7b880d5a
captynJoe <joelnjeng@gmail.com>
2023-10-06 15:50:44 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0 ; i < 10 ; i++)
+	putchar(i);
+putchar('\n');
+return (0);
+
+}
