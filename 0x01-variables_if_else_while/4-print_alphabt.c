e5a47a406ccd74fccbd49a5503c7d24cd2edbece
captynJoe <joelnjeng@gmail.com>
2023-10-06 15:24:29 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+/**
+ * main - The main function
+ * Return: always 0
+ */
+
+int main(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+	if (i != 'q' && i != 'e')
+		putchar(i);
+putchar('\n');
+return (0);
+}
