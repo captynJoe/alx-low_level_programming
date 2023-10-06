354ade5e6a909c03b06e1ae5a961910c06f5a2fc
captynJoe <joelnjeng@gmail.com>
2023-10-06 15:59:31 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *
+ * Return: Aways 0
+ *
+ */
+
+int main(void)
+{
+char i;
+
+for (i = 'z' ; i >= 'a' ; i--)
+	putchar(i);
+putchar('\n');
+return (0);
+
+}
