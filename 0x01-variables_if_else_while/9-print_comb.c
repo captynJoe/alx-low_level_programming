ed6517a7200b0a961d2092d41bdc4fd02ab9f511
captynJoe <joelnjeng@gmail.com>
2023-10-06 17:00:20 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,14 @@ int i;
 
 for (i = 0 ; i < 10 ; i++)
 {
-	if (i == 9)
-		putchar(i + '0');
-	else
-		putchar(i + '0')
+	putchar(i + '0');
+	if (i != 9)
+	{
 		putchar(',');
 		putchar(' ');
-
+	}
 }
+putchar('\n');
 return(0);
 
 }
