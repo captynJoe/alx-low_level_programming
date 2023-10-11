1a05f067d29530b4d606ad5f92dcbfdba3b4429b
captynJoe <joelnjeng@gmail.com>
2023-10-11 09:40:48 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times
+ * return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+char c;
+int i = 0;
+
+while (i <= 9)
+{
+for (c = 'a'; c < 'z'; c++)
+
+{
+_putchar(c);
+}
+_putchar('\n');
+i++;
+}
+}
