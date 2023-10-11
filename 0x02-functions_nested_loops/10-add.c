ed56a64a3eed0be35ff63a25e6b11b0d592e8446
captynJoe <joelnjeng@gmail.com>
2023-10-11 12:57:43 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Addition of values
+ * @n1: First computed no.
+ * @n2: Second computed no.
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
