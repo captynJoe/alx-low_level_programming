054e85e768812f1fc3944206cf4bb9d720c7e628
captynJoe <joelnjeng@gmail.com>
2023-10-11 09:26:13 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - The main part
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+
