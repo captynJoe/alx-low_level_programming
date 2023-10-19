e855728d3ee0b68afb9d7d7e676b00cd290624d6
captynJoe <joelnjeng@gmail.com>
2023-10-19 15:13:44 -0700

    dfa

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 /**
  * main - Entry point of the program
- *
- * Description:
- * - Declares an integer variable n, an array of integers a with 5 elements
- * - Sets the value of the third element in array a (a[2]) to 1024.
- * - Points the pointer p to the address of the integer variable n.
- * - Modifies the value of the integer variable n
- * - Prints the modified value of a[2] to the standard output.
- * Return: 0 to indicate successful execution of the program.
  */
 
 int main(void)
