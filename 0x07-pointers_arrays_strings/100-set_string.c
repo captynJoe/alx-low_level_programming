c6609c27e6c6af44c0aaa92ec97a2307a9e85a23
captynJoe <joelnjeng@gmail.com>
2023-10-23 16:34:54 -0700

    fdaf

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,18 +1,13 @@
 #include "main.h"
 
 /**
-  * set_string - main function
-  *
-  * @s: Function parameter
-  *
-  * @to: Function parameter
-  *
-  * Return: Always 0.
-  */
-void set_string(char **s, char *to)
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer
+ * @to: pointer char
+ */
 
+void set_string(char **s, char *to)
 {
 	*s = to;
-
 }
 
