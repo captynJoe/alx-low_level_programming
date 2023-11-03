75cce371b78fc2e782f872fb7aebb0a329c53283
captynJoe <joelnjeng@gmail.com>
2023-11-02 17:26:52 -0700

    adfa

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -73,7 +73,6 @@ int check_for_digits(char **av)
 {
 int i, j;
 
-
 for (i = 1; i < 3; i++)
 {
 for (j = 0; av[i][j]; j++)
@@ -84,7 +83,6 @@ return (1);
 }
 return (0);
 }
-
 
 /**
 * init - initializes a string
@@ -102,7 +100,6 @@ for (i = 0; i < l; i++)
 str[i] = '0';
 str[i] = '\0';
 }
-
 
 /**
 * main - multiply two numbers
@@ -127,11 +124,9 @@ _putchar(e[ti]);
 exit(98);
 }
 for (l1 = 0; argv[1][l1]; l1++)
-       ;
 for (l2 = 0; argv[2][l2]; l2++)
-       ;
 ln = l1 + l2 + 1;
-a = malloc(ln * sizeof(char));
+a = malloc(ln *sizeof(char));
 if (a == NULL)
 {
 for (ti = 0; e[ti]; ti++)
@@ -146,7 +141,7 @@ if (t == NULL)
 {
 for (ti = 0; e[ti]; ti++)
 _putchar(e[ti]);
-free(a); 
+free(a);
 exit(98);
 }
 }
