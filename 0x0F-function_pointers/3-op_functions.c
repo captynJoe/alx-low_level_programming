41c1c4862d636e10a77c2c200703e522e9a90182
captynJoe <joelnjeng@gmail.com>
2023-11-08 16:33:54 -0800

    asd

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,10 +1,12 @@
 #include "3-calc.h"
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);	
-int op_div(int a, int b);
-int op_mod(int a, int b);
+
+	int op_add(int a, int b);
+	int op_sub(int a, int b);
+	int op_mul(int a, int b);
+	int op_div(int a, int b);
+	int op_mod(int a, int b);
+
 
 /**
  * op_add - Returns the sum of two numbers.
@@ -13,10 +15,10 @@ int op_mod(int a, int b);
  *
  * Return: The sum of a and b.
  */
-int op_add(int a, int b)
-{
-	return (a + b);
-}
+	int op_add(int a, int b)
+	{
+		return (a + b);
+	}
 /**
  * op_sub - Returns the difference of two numbers.
  * @a: The first number.
@@ -24,10 +26,10 @@ int op_add(int a, int b)
  *
  * Return: The difference of a and b.
  */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
+	int op_sub(int a, int b)
+	{
+		return (a - b);
+	}
 /**
  * op_mul - Returns the product of two numbers.
  * @a: The first number.
@@ -35,10 +37,10 @@ int op_sub(int a, int b)
  *
  * Return: The product of a and b.
  */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
+	int op_mul(int a, int b)
+	{
+		return (a * b);
+	}
 /**
  * op_div - Returns the division of two numbers.
  * @a: The first number.
@@ -46,10 +48,10 @@ int op_mul(int a, int b)
  *
  * Return: The quotient of a and b.
  */
-int op_div(int a, int b)
-{
-	return (a / b);
-}
+	int op_div(int a, int b)
+	{
+		return (a / b);
+	}
 /**
  * op_mod - Returns the remainder of the division of two numbers.
  * @a: The first number.
@@ -57,8 +59,8 @@ int op_div(int a, int b)
  *
  * Return: The remainder of the division of a by b.
  */
-int op_mod(int a, int b)
-{
-	return (a % b);
-}
+	int op_mod(int a, int b)
+	{
+		return (a % b);
+	}
 
