733cc3d0befe96ca0e0848cc19a993853c39c1e5
captynJoe <joelnjeng@gmail.com>
2024-01-15 16:50:44 -0800

    adfs

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <stdio.h>
 #include <stdlib.h>
 
 int _putchar(char c);
@@ -25,4 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif /* _MAIN_H_ */
